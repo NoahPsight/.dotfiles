9297c1c8c9d5ebe44081babaa0473d1b0454a70d
NoahPsight <120606151+NoahPsight@users.noreply.github.com>
2023-06-11 17:01:35 -0600

    upd

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -40,7 +40,5 @@ end, {})
 vim.keymap.set('n', '<leader>,f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>,b', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>,s', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>,s', builtin.live_grep, {})
 
