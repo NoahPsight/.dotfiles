e98b98eef52b14956da176c02466daeb01906402
NoahPsight <120606151+NoahPsight@users.noreply.github.com>
2023-06-18 01:31:33 -0600

    upd

--- scripts/p.sh
+++ scripts/p.sh
@@ -2,6 +2,7 @@
 
 function p() {
     local cmd="$1"
+    declare -A seen
     local projects=()
     if [[ "$cmd" == "-a" ]]; then
         if [[ -z "$2" ]]; then
@@ -14,7 +15,8 @@ function p() {
     if [[ "$cmd" == "-d" ]]; then
         sel=$(cat ~/.projects | fzf)
         if [[ -n "$sel" ]]; then
-            sed -i "/$sel/d" ~/.projects
+            grep -Fvx "$sel" ~/.projects > ~/.projects_temp && mv ~/.projects_temp ~/.projects
+            grep -Fvx "$sel" ~/.cache/.project_last > ~/.cache/.project_last_temp && mv ~/.cache/.project_last_temp ~/.cache/.project_last
         fi
         return 0
     fi
@@ -29,12 +31,27 @@ function p() {
             projects+=("$line")
         fi
     done < ~/.projects
+    local recent_projects=()
+    if [[ -f ~/.cache/.project_last ]]; then
+        while IFS= read -r line; do
+            recent_projects+=("$line")
+        done < ~/.cache/.project_last
+    fi
+    local combined_projects=("${recent_projects[@]}" "${projects[@]}")
+    local unique_projects=()
+    for proj in "${combined_projects[@]}"; do
+        if [[ -n "${seen[$proj]}" ]]; then
+            continue
+        fi
+        unique_projects+=("$proj")
+        seen["$proj"]=1
+    done
     case "$cmd" in
         -l)
-            printf '%s\n' "${projects[@]}"
+            printf '%s\n' "${unique_projects[@]}"
             ;;
         *)
-            local dir=$(printf '%s\n' "${projects[@]}" | fzf +m)
+            local dir=$(printf '%s\n' "${unique_projects[@]}" | fzf )
             if [[ -n "$dir" ]]; then
                 local tmux_session_name=$(echo "$dir" | tr '.' '_')
                 if ! tmux list-sessions | grep -q "^$tmux_session_name:"; then
@@ -47,6 +64,8 @@ function p() {
                 else
                     tmux attach-session -t "$tmux_session_name"
                 fi
+                grep -Fvx "$dir" ~/.cache/.project_last > ~/.cache/.project_last_temp && mv ~/.cache/.project_last_temp ~/.cache/.project_last
+                echo -e "$dir\n$(cat ~/.cache/.project_last)" > ~/.cache/.project_last
             fi
             ;;
     esac
