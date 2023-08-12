ae3d1de51cbb1df3ae10ddfee559ffe21ce4c21a
NoahPsight <120606151+NoahPsight@users.noreply.github.com>
2023-08-11 17:22:40 -0700

    fix(windowprefs.json): change width from 1914 to 853 to adjust window size feat(0BDFDB.config.json): update BDFDB version to 3.3.3 fix(0BDFDB.data.json): remove unused FileRequestUtils props fix(0BDFDB.plugin.js): update BDFDB version to 3.3.3 and fix getFileData function fix(bgrng.sh): add support for overriding quote with a file named override.txt in quotes directory

--- scripts/bgrng.sh
+++ scripts/bgrng.sh
@@ -1,4 +1,8 @@
-FILE=$(find ~/wallpaper -type f | shuf -n 1);
-QUOTE=$(find ~/quotes -type f | shuf -n 1 | xargs cat);
-convert "$FILE" -blur 0x8 -font "Z003-MediumItalic" -fill white -pointsize 50 -gravity center -annotate +0+0 "$QUOTE" ~/current_wallpaper.png 2>/dev/null;
+FILE=$(find ~/wallpaper -type f | shuf -n 1)
+if [ -f ~/quotes/override.txt ]; then
+  QUOTE=$(cat ~/quotes/override.txt)
+else
+  QUOTE=$(find ~/quotes -type f | grep -v "override.txt" | shuf -n 1 | xargs cat)
+fi
+convert "$FILE" -blur 0x8 -font "Z003-MediumItalic" -fill white -pointsize 50 -gravity center -annotate +0+0 "$QUOTE" ~/current_wallpaper.png 2>/dev/null
 feh --bg-fill ~/current_wallpaper.png
