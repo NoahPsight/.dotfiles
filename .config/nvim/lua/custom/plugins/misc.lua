c5d1c69546f7e383ea3bffd1e4ae370ffd6f9a14
NoahPsight <120606151+NoahPsight@users.noreply.github.com>
2024-01-10 16:27:25 -0800

    upd

--- .config/nvim/lua/custom/plugins/misc.lua
+++ .config/nvim/lua/custom/plugins/misc.lua
@@ -1,1 +1,28 @@
-return {}
+return {
+  {
+    "johmsalas/text-case.nvim",
+    lazy = false,
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup {}
+      require("telescope").load_extension "textcase"
+    end,
+    init = function()
+      local map = require "custom.mappings"
+      map.text_case = {
+        n = {
+          ["ga."] = {
+            "<cmd>TextCaseOpenTelescope<CR>",
+            desc = "Telescope",
+          },
+        },
+        v = {
+          ["ga."] = {
+            "<cmd>TextCaseOpenTelescope<CR>",
+            desc = "Telescope",
+          },
+        },
+      }
+    end,
+  },
+}
